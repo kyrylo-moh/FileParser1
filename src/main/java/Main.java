38c85188cbfa93bbd27b260cd51cd30887d1822d
kimoe <kimo.evil@gmail.com>
2019-10-26 11:35:16 +0300

    Added some packages and classes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,11 @@
+import controller.Controller;
+
 import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        FileParser fileParser = new FileParser();
-        //fileParser.countStrEntriesFile("D:\\fileParserTest.txt", "1");
-        fileParser.replaceStringFile("D:\\fileParserTest.txt", "1", "000");
-
+    public static void main(String[] args) {
+            new Controller().repeat();
     }
 
 }
